192e44c4a6e7c5314e6fec4b21053568ee985f9d
Joseph Pan <m@hahack.com>
2018-03-21 23:23:36 +0800

    解决 #2

--- client/stt.py
+++ client/stt.py
@@ -492,7 +492,7 @@ class ALiBaBaSTT(AbstractSTTEngine):
         return hash.digest().encode('base64').strip()
 
     def to_sha1_base64(self, stringToSign, secret):
-        hmacsha1 = hmac.new(secret, stringToSign, hashlib.sha1)
+        hmacsha1 = hmac.new(str(secret), str(stringToSign), hashlib.sha1)
         return base64.b64encode(hmacsha1.digest())
 
     def transcribe(self, fp):
