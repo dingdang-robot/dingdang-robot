7920790a352b2979ef2a2fe0d90c4f941dcff123
mssssir <mcxr4299@gmail.com>
2018-04-07 22:03:40 +0800

    增加听不到时不说话的配置

--- client/conversation.py
+++ client/conversation.py
@@ -86,6 +86,8 @@ class Conversation(object):
             pixels.think()
             if input:
                 self.brain.query(input, self.wxbot)
-            else:
-                self.mic.say("什么?")
+            elif 'shut_up_if_no_input' in self.profile and \
+                 not self.profile['shut_up_if_no_input']:
+                self.mic.say(u"什么?")
             pixels.off()
+
